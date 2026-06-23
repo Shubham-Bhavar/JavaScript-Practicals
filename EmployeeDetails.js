bf1278dd9d8f70cff52623f6287b2b9acb4abf09
Shubham Balasaheb Bhavar <shubhambhavar108@gmail.com>
2026-06-23 12:46:40 +0530

    Create EmployeeDetails.js

---
+++ EmployeeDetails.js
@@ -0,0 +1,7 @@
+var empName="Shubham";
+var empSalary=25000;
+var empAge=20;
+console.log("Employee Inforamation\n");
+console.log("Employee Name:"empName);
+console.log("Employee Salary:"empSalary);
+console.log("Employee Age:"empAge);
