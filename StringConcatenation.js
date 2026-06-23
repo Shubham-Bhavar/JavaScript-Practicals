1513fe3f845693ce2497b880962b6423007e7c2b
Shubham Balasaheb Bhavar <shubhambhavar108@gmail.com>
2026-06-23 13:56:21 +0530

    Create StringConcatenation.js

---
+++ StringConcatenation.js
@@ -0,0 +1,8 @@
+// Store first name
+var myName = "Shubham";
+
+// Store surname
+var mySurname = "Bhavar";
+
+// Concatenate first name and surname with a space
+console.log(myName + " " + mySurname);
