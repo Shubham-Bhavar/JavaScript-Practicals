505d17991cb95a7d3b2fe8b06103afab88d1d5c6
Shubham Balasaheb Bhavar <shubhambhavar108@gmail.com>
2026-06-24 14:57:50 +0530

    Create FactorialOfNum.js

---
+++ FactorialOfNum.js
@@ -0,0 +1,11 @@
+// Program to find factorial of a number
+
+let num = 5;
+let factorial = 1;
+
+for (let i = 1; i <= num; i++) {
+
+    factorial = factorial * i;
+}
+
+console.log("Factorial = " + factorial);
