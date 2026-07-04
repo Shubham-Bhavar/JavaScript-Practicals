a29c4b13ee50be6603e88b9ce0815a28d874f350
Shubham Balasaheb Bhavar <shubhambhavar108@gmail.com>
2026-07-04 19:50:40 +0530

    Add methods.js to check string presence and boundaries

---
+++ methods.js
@@ -0,0 +1,6 @@
+// ❓ check presence, start, and end
+let str = "hello world";
+
+console.log(str.includes("world"));   // true
+console.log(str.startsWith("hello")); // true
+console.log(str.endsWith("world"));   // true
