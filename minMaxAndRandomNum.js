caf5f0dd1383d0a43e35209ce8ae82640de2a94f
Shubham Balasaheb Bhavar <shubhambhavar108@gmail.com>
2026-07-07 17:19:07 +0530

    Create minMaxAndRandomNum.js

---
+++ minMaxAndRandomNum.js
@@ -0,0 +1,10 @@
+// Q3: Find max, min and random number
+let a = 10, b = 25;
+
+let max = Math.max(a, b);
+let min = Math.min(a, b);
+let random = Math.random();
+
+console.log("Max:", max);
+console.log("Min:", min);
+console.log("Random:", random);
