f7451b4cf9cd92060ce2e6008bb0417471f3aa6a
Shubham Balasaheb Bhavar <shubhambhavar108@gmail.com>
2026-07-07 17:09:08 +0530

    Create squareAndCubeUsingMathFunction.js

---
+++ squareAndCubeUsingMathFunction.js
@@ -0,0 +1,8 @@
+// Q1: Find square and cube using Math functions
+let num = 5;
+
+let square = Math.pow(num, 2);
+let cube = Math.pow(num, 3);
+
+console.log("Square:", square);
+console.log("Cube:", cube);
