ebae4f5ec492e63d0c7f728c41ac681c31bdc28a
Shubham Balasaheb Bhavar <shubhambhavar108@gmail.com>
2026-07-07 17:13:01 +0530

    Add square root and absolute value calculations

---
+++ squareRootAndAbsValueOfNum.js
@@ -0,0 +1,8 @@
+// Q2: Find square root and absolute value
+let num = -16;
+
+let sqrt = Math.sqrt(16);
+let abs = Math.abs(num);
+
+console.log("Square Root:", sqrt);
+console.log("Absolute Value:", abs);
